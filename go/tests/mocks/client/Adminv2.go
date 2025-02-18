49d467ba7afeafc367817293ae4437778c96fef8
Stefan Majer <stefan.majer@f-i-ts.de>
2025-02-18 17:24:58 +0100

    split query to make it reusable between api and admin services

--- go/tests/mocks/client/Adminv2.go
+++ go/tests/mocks/client/Adminv2.go
@@ -33,6 +33,26 @@ func (_m *Adminv2) Filesystem() adminv2connect.FilesystemServiceClient {
 	return r0
 }
 
+// IP provides a mock function with no fields
+func (_m *Adminv2) IP() adminv2connect.IPServiceClient {
+	ret := _m.Called()
+
+	if len(ret) == 0 {
+		panic("no return value specified for IP")
+	}
+
+	var r0 adminv2connect.IPServiceClient
+	if rf, ok := ret.Get(0).(func() adminv2connect.IPServiceClient); ok {
+		r0 = rf()
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).(adminv2connect.IPServiceClient)
+		}
+	}
+
+	return r0
+}
+
 // Partition provides a mock function with no fields
 func (_m *Adminv2) Partition() adminv2connect.PartitionServiceClient {
 	ret := _m.Called()
