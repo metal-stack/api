026ec221dacec0e7351a0b230ae0345e192052b7
Stefan Majer <stefan.majer@f-i-ts.de>
2025-06-30 16:01:51 +0200

    python example 2 is working now

---
+++ examples/python/ip-list2.py
@@ -0,0 +1,32 @@
+# client.py
+from connecpy.context import ClientContext
+from connecpy.exceptions import ConnecpyServerException
+import os
+import metalstack.api.v2.ip_connecpy as ip_connecpy
+import metalstack.api.v2.ip_pb2 as ip_pb2
+
+
+timeout_s = 5
+baseurl = os.environ['METAL_APISERVER_URL']
+token = os.environ['API_TOKEN']
+project = os.environ['PROJECT_ID']
+
+def main():
+    with ip_connecpy.IPServiceClient(baseurl, timeout=timeout_s) as client:
+        try:
+            response = client.List(
+                ctx=ClientContext(),
+                request=ip_pb2.IPServiceListRequest(project=project),
+                headers={
+                    "Authorization": "Bearer " + token,
+                }
+            )
+            for ip in response.ips:
+                print(ip.ip, ip.name, ip.project, ip.network)
+            # print(response)
+        except ConnecpyServerException as e:
+            print(e.code, e.message, e.to_dict())
+
+
+if __name__ == "__main__":
+    main()
