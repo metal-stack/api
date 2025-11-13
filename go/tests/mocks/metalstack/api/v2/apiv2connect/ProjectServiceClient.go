c22c41cd39533143670af89739830fc5a12b7bb2
Marcel Arndt <33572038+mac641@users.noreply.github.com>
2025-11-13 08:17:58 +0100

    ensure ProjectServiceRemoveMemberRequest can be triggered as PROJECT_ROLE_VIEWER (#67)

--- go/tests/mocks/metalstack/api/v2/apiv2connect/ProjectServiceClient.go
+++ go/tests/mocks/metalstack/api/v2/apiv2connect/ProjectServiceClient.go
@@ -255,6 +255,36 @@ func (_m *ProjectServiceClient) InvitesList(_a0 context.Context, _a1 *apiv2.Proj
 	return r0, r1
 }
 
+// Leave provides a mock function with given fields: _a0, _a1
+func (_m *ProjectServiceClient) Leave(_a0 context.Context, _a1 *apiv2.ProjectServiceLeaveRequest) (*apiv2.ProjectServiceLeaveResponse, error) {
+	ret := _m.Called(_a0, _a1)
+
+	if len(ret) == 0 {
+		panic("no return value specified for Leave")
+	}
+
+	var r0 *apiv2.ProjectServiceLeaveResponse
+	var r1 error
+	if rf, ok := ret.Get(0).(func(context.Context, *apiv2.ProjectServiceLeaveRequest) (*apiv2.ProjectServiceLeaveResponse, error)); ok {
+		return rf(_a0, _a1)
+	}
+	if rf, ok := ret.Get(0).(func(context.Context, *apiv2.ProjectServiceLeaveRequest) *apiv2.ProjectServiceLeaveResponse); ok {
+		r0 = rf(_a0, _a1)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).(*apiv2.ProjectServiceLeaveResponse)
+		}
+	}
+
+	if rf, ok := ret.Get(1).(func(context.Context, *apiv2.ProjectServiceLeaveRequest) error); ok {
+		r1 = rf(_a0, _a1)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
 // List provides a mock function with given fields: _a0, _a1
 func (_m *ProjectServiceClient) List(_a0 context.Context, _a1 *apiv2.ProjectServiceListRequest) (*apiv2.ProjectServiceListResponse, error) {
 	ret := _m.Called(_a0, _a1)
