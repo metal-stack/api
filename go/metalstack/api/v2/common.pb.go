657a68bcbcbee1e693fddb52c113dd5dfecab967
Gerrit <tirreg91@googlemail.com>
2026-04-02 14:10:45 +0200

    Add CEL validation.

--- go/metalstack/api/v2/common.pb.go
+++ go/metalstack/api/v2/common.pb.go
@@ -874,7 +874,7 @@ var File_metalstack_api_v2_common_proto protoreflect.FileDescriptor
 
 const file_metalstack_api_v2_common_proto_rawDesc = "" +
 	"\n" +
-	"\x1emetalstack/api/v2/common.proto\x12\x11metalstack.api.v2\x1a\x1bbuf/validate/validate.proto\x1a google/protobuf/descriptor.proto\x1a\x1fgoogle/protobuf/timestamp.proto\"O\n" +
+	"\x1emetalstack/api/v2/common.proto\x12\x11metalstack.api.v2\x1a\x1bbuf/validate/validate.proto\x1a google/protobuf/descriptor.proto\x1a\x1fgoogle/protobuf/timestamp.proto\x1a(metalstack/api/v2/predefined_rules.proto\"O\n" +
 	"\x06Paging\x12\x17\n" +
 	"\x04page\x18\x01 \x01(\x04H\x00R\x04page\x88\x01\x01\x12\x19\n" +
 	"\x05count\x18\x02 \x01(\x04H\x01R\x05count\x88\x01\x01B\a\n" +
@@ -894,10 +894,10 @@ const file_metalstack_api_v2_common_proto_rawDesc = "" +
 	"\n" +
 	"generation\x18\x04 \x01(\x04R\n" +
 	"generationB\t\n" +
-	"\a_labels\"x\n" +
+	"\a_labels\"\x86\x01\n" +
 	"\fUpdateLabels\x121\n" +
-	"\x06update\x18\x01 \x01(\v2\x19.metalstack.api.v2.LabelsR\x06update\x12\x16\n" +
-	"\x06remove\x18\x02 \x03(\tR\x06remove\x12\x1d\n" +
+	"\x06update\x18\x01 \x01(\v2\x19.metalstack.api.v2.LabelsR\x06update\x12$\n" +
+	"\x06remove\x18\x02 \x03(\tB\f\xbaH\t\x92\x01\x06Ф\xb3\xb1\x02\x01R\x06remove\x12\x1d\n" +
 	"\n" +
 	"remove_all\x18\x03 \x01(\bR\tremoveAll\"\xaa\x01\n" +
 	"\n" +
@@ -1024,6 +1024,7 @@ func file_metalstack_api_v2_common_proto_init() {
 	if File_metalstack_api_v2_common_proto != nil {
 		return
 	}
+	file_metalstack_api_v2_predefined_rules_proto_init()
 	file_metalstack_api_v2_common_proto_msgTypes[0].OneofWrappers = []any{}
 	file_metalstack_api_v2_common_proto_msgTypes[2].OneofWrappers = []any{}
 	type x struct{}
