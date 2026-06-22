9b133ec9fd5cef90c9b42a8f40f1e754776cb295
Gerrit <tirreg91@googlemail.com>
2026-06-22 09:07:28 +0200

    Remove cluster description tag that we do not use actively.

--- go/tag/cluster.go
+++ go/tag/cluster.go
@@ -5,8 +5,6 @@ const (
 	ClusterID = "cluster.metal-stack.io/id"
 	// ClusterName tag to store the name of the cluster
 	ClusterName = "cluster.metal-stack.io/name"
-	// ClusterDescription tag to store the description of the cluster
-	ClusterDescription = "cluster.metal-stack.io/description"
 	// ClusterProject tag to store the project the cluster belongs to
 	ClusterProject = "cluster.metal-stack.io/project"
 	// ClusterPartition tag to store the partition of the cluster
